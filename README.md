b083b816eafd872b2e72ecdb9cd26945e8f6d6d1
Marcelo <marcelo.website.us@gmail.com>
2017-07-24 23:04:58 -0400

    initial changes in admin side_nav

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-#blueprints
+esse e a aplication que eu estou construindo, para acessar 
+de casa use http://localhost/blueprints/public/api/allPrograms
+ou pra ir pro admin http://localhost/blueprints/admin/ ou pra 
+ir pro front end
---
+++ admin/includes/side_nav.php
@@ -0,0 +1,40 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <h3>EDIT PROGRAM</h3>
+        <li>
+            <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Contacts</a>
+        </li>
+
+        <li>Program Name</li> <br>
+        <li>Designer/Evaluator Contact</li> <br>
+        <li>Program Information Contact</li>
+
+        <li>
+            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Target Population</a>
+        </li>
+        <li>Population Demographics (narrative)</li> <br>
+        <li>Age</li> <br>
+        <li>Gender</li>
+        <li>Race/Ethnicity</li> <br>
+        <li>Program Recipients</li> <br>
+        <li>Continuum of Intervention</li>
+        <li>
+            <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Description</a>
+        </li>
+        <li>
+            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Program Type/Setting</a>
+        </li>
+        <li class="active">
+            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Theoretical</a>
+        </li>
+        <li>
+            <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Risk/Protective Factors</a>
+        </li>
+        <li class="active">
+            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Studies</a>
+        </li>
+        <li>
+            <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Continuum of Intervention</a>
+        </li>
+    </ul>
+</div>
